d4cbc9fb1df5376e4d94aecc5c357d584c4b722c
Logan Shoemaker <logansshoemaker@gmail.com>
2019-09-06 07:46:14 -0600

    updating tiles when user clicks

--- src/Board.tsx
+++ src/Board.tsx
@@ -1,47 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 // Use hooks
-// Use typescript
 // Write tests
 // Allow users to change their player name
-
-const Board: React.FC = () => {
-  /*
-
-  TODO: method (hook?) toggle player 1 & player 2
-
-  */
-
-  /*
-
-  TODO: method (hook?) update places & re-render board when space selected
-
-  */
-
-  /*
+/*
 
   TODO: method(s?) (hook?) to calculate state of board (after x turns?)
 
   */
 
-  /*
+/*
 
   TODO: separate Tile functional component to present tile
 
   */
 
-  /*
+/*
 
   TODO: option at start of game to allow users to pick their player name
 
   */
 
+const Board: React.FC = () => {
   interface Tile {
     player: string;
   }
 
-  const places: Array<Array<Tile>> = Array(6)
+  const initialBoard: Array<Array<Tile>> = Array(6)
     .fill(null)
     .map((_column: any) =>
       Array(7).fill({
@@ -49,12 +35,24 @@ const Board: React.FC = () => {
       })
     );
 
+  const [currentPlayer, toggleCurrentPlayer1] = useState("player1");
+  const [places, updatePlaces] = useState(initialBoard);
+
+  const handleTileClick = (rowIndex: number, columnIndex: number) => {
+    const newPlaces = [...places];
+    newPlaces[rowIndex][columnIndex] = { player: currentPlayer };
+    updatePlaces(newPlaces);
+    toggleCurrentPlayer1(currentPlayer === "player1" ? "player2" : "player1");
+  };
+
   // onClick of tile will send rowIndex and columnIndex to update places via hook
   return (
     <div>
+      <h1>{currentPlayer}</h1>
       {places.map((row, rowIndex) => {
         return (
           <div
+            key={`row${rowIndex}`}
             style={{
               display: "block"
             }}
@@ -62,6 +60,11 @@ const Board: React.FC = () => {
             {row.map((column: Tile, columnIndex: number) => {
               return (
                 <div
+                  onClick={() =>
+                    column.player === "OPEN" &&
+                    handleTileClick(rowIndex, columnIndex)
+                  }
+                  key={`Tile${columnIndex}`}
                   style={{
                     display: "inline-block",
                     width: "75px",
@@ -70,6 +73,8 @@ const Board: React.FC = () => {
                   }}
                 >
                   {column.player}
+                  <br />
+                  {columnIndex}
                 </div>
               );
             })}
