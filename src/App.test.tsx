0716bf92ac24bbf99e4cb343fb4499d3c5fe3b1c
Logan Shoemaker <logansshoemaker@gmail.com>
2019-09-09 14:00:49 -0600

    unit tests for calculations

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,6 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { Board, ColorSelect, Column, Tile } from "./components";
+import {
+  calculateArray,
+  calculateHorizontal,
+  calculateDiagonallyUp,
+  calculateDiagonallyDown
+} from "./functions";
 
 it("App renders without crashing", () => {
   const div = document.createElement("div");
@@ -38,4 +44,86 @@ it("Tile renders without crashing", () => {
   const div = document.createElement("div");
   ReactDOM.render(<Tile player="RED" colorTile={() => {}} />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it("Calculate a vertical match", () => {
+  const passingArray = [
+    { player: "red" },
+    { player: "red" },
+    { player: "red" },
+    { player: "red" }
+  ];
+  const failingArray = [
+    { player: "red" },
+    { player: "black" },
+    { player: "red" },
+    { player: "red" }
+  ];
+  expect(calculateArray(passingArray)).toBeTruthy();
+  expect(calculateArray(failingArray)).toBeFalsy();
+});
+
+it("Calculates a horizontal match", () => {
+  const passingArray = [
+    [{}, {}, {}, {}, {}, {}],
+    [{}, {}, {}, {}, {}, { player: "red" }],
+    [{}, {}, {}, {}, {}, { player: "red" }],
+    [{}, {}, {}, {}, {}, { player: "red" }],
+    [{}, {}, {}, {}, {}, { player: "red" }],
+    [{}, {}, {}, {}, {}, {}]
+  ];
+  const failingArray = [
+    [{}, {}, {}, {}, {}, {}],
+    [{}, {}, {}, {}, {}, { player: "red" }],
+    [{}, {}, {}, {}, {}, { player: "red" }],
+    [{}, {}, {}, {}, {}, {}],
+    [{}, {}, {}, {}, {}, { player: "red" }],
+    [{}, {}, {}, {}, {}, {}]
+  ];
+  expect(calculateHorizontal(passingArray)).toBeTruthy();
+  expect(calculateHorizontal(failingArray)).toBeFalsy();
+});
+
+it("Calculates an upward diagonal match", () => {
+  const passingArray = [
+    [{ player: "red" }, {}, {}, {}, {}, {}],
+    [{}, { player: "red" }, {}, {}, {}, {}],
+    [{}, {}, { player: "red" }, {}, {}, {}],
+    [{}, {}, {}, { player: "red" }, {}, {}],
+    [{}, {}, {}, {}, {}, {}],
+    [{}, {}, {}, {}, {}, {}]
+  ];
+  const failingArray = [
+    [{}, {}, {}, {}, {}, { player: "red" }],
+    [{}, {}, {}, {}, { player: "red" }, {}],
+    [{}, {}, {}, { player: "red" }, {}, {}],
+    [{}, {}, { player: "red" }, {}, {}, {}],
+    [{}, {}, {}, {}, {}, {}],
+    [{}, {}, {}, {}, {}, {}]
+  ];
+
+  expect(calculateDiagonallyUp(passingArray)).toBeTruthy();
+  expect(calculateDiagonallyUp(failingArray)).toBeFalsy();
+});
+
+it("Calculates a downward diagonal match", () => {
+  const passingArray = [
+    [{}, {}, {}, {}, {}, { player: "red" }],
+    [{}, {}, {}, {}, { player: "red" }, {}],
+    [{}, {}, {}, { player: "red" }, {}, {}],
+    [{}, {}, { player: "red" }, {}, {}, {}],
+    [{}, {}, {}, {}, {}, {}],
+    [{}, {}, {}, {}, {}, {}]
+  ];
+  const failingArray = [
+    [{ player: "red" }, {}, {}, {}, {}, {}],
+    [{}, { player: "red" }, {}, {}, {}, {}],
+    [{}, {}, { player: "red" }, {}, {}, {}],
+    [{}, {}, {}, { player: "red" }, {}, {}],
+    [{}, {}, {}, {}, {}, {}],
+    [{}, {}, {}, {}, {}, {}]
+  ];
+
+  expect(calculateDiagonallyDown(passingArray)).toBeTruthy();
+  expect(calculateDiagonallyDown(failingArray)).toBeFalsy();
 });
--- src/interfaces/place.ts
+++ src/interfaces/place.ts
@@ -1,3 +1,3 @@
 export default interface place {
-  player: string;
+  player?: string;
 }
