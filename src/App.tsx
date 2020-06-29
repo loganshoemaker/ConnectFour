5a2476b4d3c239651747b0bf64f4651748f02fde
Logan Shoemaker <logansshoemaker@gmail.com>
2020-06-29 17:15:35 -0600

    Centering

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Board } from "./components/Board";
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <Board />
+    </div>
+  );
+};
+
+export default App;
